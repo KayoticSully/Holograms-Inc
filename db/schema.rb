4dc44a3097a6114bf582926eff4698b99bb9591f
Ryan Sullivan <KayoticSully@gmail.com>
2012-10-07 19:23:22 -0400

    Added Some User Permissions
    Permission Flags for:
    - purchase
    - products_edit
    - products_quantity
    - help_edit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121007231009) do
+ActiveRecord::Schema.define(:version => 20121007232200) do
 
   create_table "groups", :force => true do |t|
     t.integer  "product_id"
@@ -66,8 +66,12 @@ ActiveRecord::Schema.define(:version => 20121007231009) do
 
   create_table "user_types", :force => true do |t|
     t.boolean  "products"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+    t.boolean  "purchase"
+    t.boolean  "products_edit"
+    t.boolean  "products_quantity"
+    t.boolean  "help_edit"
   end
 
   create_table "users", :force => true do |t|
