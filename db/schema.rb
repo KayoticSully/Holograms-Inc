dfd151e76368b920c83f6c74675fece225fce90a
Ryan Sullivan <KayoticSully@gmail.com>
2012-10-07 20:14:02 -0400

    Fixed Schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -12,14 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121007232200) do
-
-  create_table "groups", :force => true do |t|
-    t.integer  "product_id"
-    t.integer  "keyword_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
+ActiveRecord::Schema.define(:version => 20121007235445) do
 
   create_table "help_items", :force => true do |t|
     t.string   "title"
@@ -80,7 +72,7 @@ ActiveRecord::Schema.define(:version => 20121007232200) do
     t.string   "last_name"
     t.string   "address"
     t.string   "city"
-    t.integer  "zipcode"
+    t.string   "zipcode"
     t.string   "email_address"
     t.string   "hashed_password"
     t.string   "credit_card"
@@ -94,90 +86,3 @@ ActiveRecord::Schema.define(:version => 20121007232200) do
   end
 
 end
-=======
-# encoding: UTF-8
-# This file is auto-generated from the current state of the database. Instead
-# of editing this file, please use the migrations feature of Active Record to
-# incrementally modify your database, and then regenerate this schema definition.
-#
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
-#
-# It's strongly recommended to check this file into your version control system.
-
-ActiveRecord::Schema.define(:version => 20121007235445) do
-
-  create_table "groups", :force => true do |t|
-    t.integer  "product_id"
-    t.integer  "keyword_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "help_items", :force => true do |t|
-    t.string   "title"
-    t.text     "text"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "keywords", :force => true do |t|
-    t.string   "name"
-    t.integer  "under"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "order_items", :force => true do |t|
-    t.integer  "order_id"
-    t.decimal  "price"
-    t.integer  "product_id"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "orders", :force => true do |t|
-    t.integer  "user_id"
-    t.decimal  "total_price"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
-  create_table "products", :force => true do |t|
-    t.string   "name"
-    t.text     "description"
-    t.decimal  "price"
-    t.string   "image"
-    t.integer  "stock"
-    t.boolean  "public"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
-  create_table "user_types", :force => true do |t|
-    t.boolean  "products"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  create_table "users", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "address"
-    t.string   "city"
-    t.string   "zipcode"
-    t.string   "email_address"
-    t.string   "hashed_password"
-    t.string   "credit_card"
-    t.string   "phone_number"
-    t.integer  "user_type_id"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.string   "password_salt"
-  end
-
-end
->>>>>>> Added field validations
