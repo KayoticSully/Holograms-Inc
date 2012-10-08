8906112a9f760bd298a3fbfad4b7feacb1ad32a6
Shane Steinert <smsteinert@msn.com>
2012-10-07 20:48:43 -0400

    Updated phone number to make it optional

--- config/initializers/validators/PhoneValidator.rb
+++ config/initializers/validators/PhoneValidator.rb
@@ -2,7 +2,7 @@ class PhoneNumberValidator < ActiveModel::EachValidator
   
   #validate zipcode if country is US, else return zip
   def validate_each(record,attribute,value)
-    unless value =~ /\A\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})\Z/
+    unless value =~ /\A\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})\Z/ || value == ""
       record.errors[attribute] << 'is not a valid phone number'
     end
   end
