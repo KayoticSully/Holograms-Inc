9e44c2c0b3bd03fc3e7c6ae10bc26879c1622d92
Ryan Sullivan <KayoticSully@gmail.com>
2012-10-13 23:41:07 -0400

    Made cart quantity number update in the menubar.  Clicking that text now also links to the cart edit page

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,14 @@ class Order < ActiveRecord::Base
   belongs_to :user
   
   attr_accessible :total_price, :user_id, :purchased
+  
+  def size
+    size = 0
+    
+    order_items.each do |order_item|
+      size += order_item.quantity
+    end
+    
+    size
+  end
 end
