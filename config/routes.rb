1c7be15dd19dcb51228081644e2bbd3c66e22353
Ryan Sullivan <KayoticSully@gmail.com>
2012-10-27 23:05:28 -0400

    A very small fix for the homepage

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ ProjectCourse::Application.routes.draw do
   
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => redirect('/keywords/promoted')
+  root :to => redirect('/keywords/featured')
   
   get "home/index"
   get "sessions/new"
