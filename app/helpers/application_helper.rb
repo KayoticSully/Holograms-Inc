4202f413ad5ddd0c4c0ca114b1579174dfc9c9c1
Ryan Flaherty <Ryan.Flaherty2@gmail.com>
2012-11-04 18:48:11 -0500

    Got text truncation functional.
    Added a bit of code to application_helper.rb.  I think I will use it
    more often than in one place.  Used it for the keywords/show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	#smart_truncate taken from http://stackoverflow.com/questions/1293573/rails-smart-text-truncation
+	def smart_truncate(text, char_limit)
+	  text = text.squish
+	  size = 0
+	  text.mb_chars.split().reject do |token|
+	    size+=token.size()
+	    size>char_limit
+	  end.join(" ")
+	end
 end
