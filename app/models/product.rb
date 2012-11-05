a1cc323afdb166bf63c5f2ecb1a2c72ffe42e14f
Ryan Flaherty <Ryan.Flaherty2@gmail.com>
2012-11-04 19:53:11 -0500

    I think I've fixed the overflow issues for keywords/show

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,8 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image, :name, :price, :public, :stock, :weight, :height, :length, :width, :rating, :sale_id
   
-  has_attached_file :image, :styles => { :medium => ["240x240>", :png], :small => ["50x50>", :png], :large => ["265>x280>", :png] }
+  #medium was 240x240 i changed it to 212x212 because it seemed nicer
+  has_attached_file :image, :styles => { :medium => ["212x212>", :png], :small => ["50x50>", :png], :large => ["265>x280>", :png] }
   
   has_many :groups
   has_many :keywords, :through => :groups
